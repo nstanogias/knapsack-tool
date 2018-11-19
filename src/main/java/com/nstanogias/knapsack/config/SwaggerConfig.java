1acd8792429a0ac068f847939e1d03e9e6093acd
Nikos Stanogias <nstanogias@gmail.com>
2018-11-20 00:54:23 +0100

    changed swagger config

--- src/main/java/com/nstanogias/knapsack/config/SwaggerConfig.java
+++ src/main/java/com/nstanogias/knapsack/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.nstanogias.knapsack.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -11,7 +12,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
@@ -21,7 +21,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.nstanogias.knapsack.controller"))
-                .paths(regex("/knapsack.*"))
+                .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
     }
